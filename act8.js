273888b55bd42139764f5b14be8784ff7008fca2
unishu <marlanda.mar@outlook.com>
2022-10-25 16:59:10 +1300

    mod1 lab

---
+++ act8.js
@@ -0,0 +1,77 @@
+// dealership 1 //
+
+
+const employee = { 
+    "first_name": "Amanda",
+    "second_name": "Ji",
+    "job": "Sales Manager",
+    "title": "Manager",
+    "salary": "$100000"
+}
+
+const employee2 = {
+    "first_name": "Ellen",
+    "second_name": "Ji",
+    "job": "Mechanic",
+    "title": "Head Mechanic",
+    "salary": "$83000"
+}
+
+const cars1 = {
+    "model": "CR-V",
+    "manufacturer": "Nissan",
+    "model_year": 2013,
+    "price": "$51,700"
+}
+
+const cars2 = {
+    "model": "RAV4 GX",
+    "manufacturer": "Toyota",
+    "model_year": "2022",
+    "price": "$39,300"
+}
+const cars3 = {
+    "model": "Defender",
+    "manufacturer": "Land Rover",
+    "model_year": "2020",
+    "price": "$112,900"
+}
+
+// dealership 2 //
+const em1 = { 
+    "first_name": "Amanda",
+    "second_name": "Ji",
+    "job": "Sales Manager",
+    "title": "Manager",
+    "salary": "$100000"
+}
+
+const em2 = {
+    "first_name": "Ellen",
+    "second_name": "Ji",
+    "job": "Mechanic",
+    "title": "Head Mechanic",
+    "salary": "$83000"
+}
+
+const car1 = {
+    "model": "CR-V",
+    "manufacturer": "Nissan",
+    "model_year": "2013",
+    "price": "$51,700"
+}
+
+const car2 = {
+    "model": "RAV4 GX",
+    "manufacturer": "Toyota",
+    "model_year": "2022",
+    "price": "$39,300"
+}
+const car3 = {
+    "model": "Defender",
+    "manufacturer": "Land Rover",
+    "model_year": "2020",
+    "price": "$112,900"
+}
+console.log(employee.first_name, employee.second_name);
+console.log(car2);
