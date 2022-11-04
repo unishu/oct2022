19d842fff92a10b2a7433b893a75ba44a8feac79
unishu <marlanda.mar@outlook.com>
2022-11-05 11:12:43 +1300

    mod1 labs

---
+++ mod1/act8.1.js
@@ -0,0 +1,42 @@
+// dealership 1 //
+
+const dealer1= {
+    "employee1":[
+        {"name": "Amanda Ji", "job":"Sales Manager", "title": "Miss", "salary": "$102,000"}
+    ],
+    "employee2":[
+        {"name": "Ellen Ki", "job": "Mechanic", "title": "Head miss", "salary": "$83,000"}
+    ],
+    "car1": [
+        {"model": "CR-V", "manufacturer": "Nissan", "year": 2013, "price":"$51,700"}
+    ],
+    "car2": [
+        {"model":"RAV4", "manufacturer": "Toyota", "year": 2022, "price":"$39,300"}
+    ],
+    "car3": [
+        {"model": "Defender", "manufacturer":"Land Rover", "year": 2022, "price":"$112,900"}
+    ]
+}
+
+
+const dealer2= {
+    "employee1":[
+        {"name": "Tato Penny", "job":"Sales Associate", "title": "Miss", "salary": "$100,000"}
+    ],
+    "employee2":[
+        {"name": "Otto Ryken", "job": "Project Manager", "title": "Head Mr", "salary": "$95,000"}
+    ],
+    "car1": [
+        {"model": "CX-5", "manufacturer": "Hyundai", "year": 2021, "price":"$65,000"}
+    ],
+    "car2": [
+        {"model":"Corolla", "manufacturer": "Toyota", "year": 2022, "price":"$40,300"}
+    ],
+    "car3": [
+        {"model": "Leaf", "manufacturer":"Nissan", "year": 2022, "price":"$45,000"}
+    ]
+}
+console.log(dealer1.employee1);
+console.log(dealer2.car2)
+
+
